e7ed3795adab686f3c25057d4bfa0a6f50ef8ea8
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2019-11-23 21:01:01 +0100

    replace default icons, add copyright

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <router-view
-    id="app"
-    class="mt-5"
-  />
+  <div>
+    <router-view
+      id="app"
+      class="mt-5"
+    />
+    <footer id="footer">
+      made by <b-link
+        href="https://github.com/kelzenberg"
+        rel="author"
+        target="_blank"
+      >
+        kelzenberg
+      </b-link>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
@@ -11,5 +22,17 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  text-align: center;
+  color: darkgrey;
+
+  a {
+    color: lightsteelblue;
+  }
 }
 </style>
